c79aa725bf79c9c07ea73d93065a6b0a8204f048
Rubén Álvarez <ralvarezmar@gmail.com>
2019-09-12 11:44:14 +0200

    added first person view

--- teleoperators/js/teleop.js
+++ teleoperators/js/teleop.js
@@ -9,10 +9,15 @@ $(document).ready(()=>{
     $("#spectatorCamera").click(()=>{
       var subjCamera = document.querySelector("#subjCamera");
       var spectatorCamera = document.querySelector("#primaryCamera");
-      var camera = subjCamera.getAttribute('camera','active');
-      if(camera.active===true){
+      var firstPersonCamera = document.querySelector("#firstPersonCamera");
+      var camera1 = subjCamera.getAttribute('camera','active');
+      var camera2 = spectatorCamera.getAttribute('camera','active');
+      var camera3 = firstPersonCamera.getAttribute('camera','active');
+      if(camera1.active===true){
         spectatorCamera.setAttribute('camera', 'active', true);
-      }else{
+      }else if(camera2.active===true){
+        firstPersonCamera.setAttribute('camera', 'active', true);
+      }else if(camera3.active==true){
         subjCamera.setAttribute('camera', 'active', true);
       }
     });
