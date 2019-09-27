dd51833f8d9ef1364e55f4737d6eda2226bf30f4
Rubén Álvarez <ralvarezmar@gmail.com>
2019-09-27 12:20:54 +0200

    first approximation to competitive exercises

--- JavaScript-competitive-editor/js/editor.js
+++ JavaScript-competitive-editor/js/editor.js
@@ -1,6 +1,12 @@
 import editor from './editor-methods.js'
 
-var editorRobot = 'a-pibot';
+var car1 = 'a-car1';
+var car2 = 'a-car2';
+
+var editFirst = true;
+var editSecond = false;
+var codeFirst = null;
+var codeSecond = null;
 
 $(document).ready(async ()=>{
   editor.ui = editor.setup();
@@ -10,16 +16,53 @@ $(document).ready(async ()=>{
   });
 
   $("#runbtn").click(()=>{
-    Websim.robots.executeCode(editorRobot, 'myRobot.setV(1);')
+    console.log(codeFirst);
+    Websim.robots.executeCode(car1, 'myRobot.setV(1);')
+    Websim.robots.executeCode(car2, 'myRobot.setV(-1);')
+
   });
 
   $('#resetRobot').click(()=>{
     editor.sendEvent('reset');
   });
 
+  $('#firstRobot').click(()=>{
+    if(editFirst==true){
+      codeFirst = editor.ui.getValue();
+    }
+    if(editSecond){
+      codeSecond = editor.ui.getValue();
+      editSecond=false;
+      if(codeFirst==null){
+        editor.ui.setValue("");
+      }else{
+        editor.ui.setValue(codeFirst);
+      }
+    }
+    editFirst= true;
+  });
+
+  $('#secondRobot').click(()=>{
+    if(editSecond==true){
+      codeSecond = editor.ui.getValue();
+    }
+    if(editFirst){
+      codeFirst = editor.ui.getValue();
+      editFirst=false;
+      if(codeSecond==null){
+        editor.ui.setValue("");
+      }else{
+        editor.ui.setValue(codeSecond);
+      }
+    }
+    editSecond= true;
+  });
+
   // This line executes a function to preconfigure Websim
   await Websim.config.init('../assets/config/config-duo.json');
-  var myRobot = Websim.robots.getRobotCopy('a-pibot');
+  var myRobot = Websim.robots.getRobotCopy('a-car1');
+  var myRobot2 = Websim.robots.getRobotCopy('a-car2');
   console.log(myRobot);
-  myRobot.setV(0.3);
+  // myRobot.setV(0.3);
+  // myRobot2.setV(-0.3);
 });
