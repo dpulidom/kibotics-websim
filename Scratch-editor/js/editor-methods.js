9657d17171f12e888f3d45e619483b0ddbd27009
Rubén Álvarez <ralvarezmar@gmail.com>
2019-09-29 12:34:23 +0200

    added drag bar

--- Scratch-editor/js/editor-methods.js
+++ Scratch-editor/js/editor-methods.js
@@ -20,13 +20,13 @@ export function setupBlockly(workspace){
           controls: true,
           wheel: true,
           startScale: 1.0,
-          maxScale: 3,
+          maxScale: 6,
           minScale: 0.3,
           scaleSpeed: 1.2
         },
     trashcan: true,
     horizontalLayout: false,
-    scrollbars: true
+    scrollbars: true,    
   });
 
   Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
