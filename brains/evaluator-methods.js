b363c9ba43c177ddd3c29c7ccf5b573e37f4882e
Rubén Álvarez <ralvarezmar@gmail.com>
2019-10-29 19:54:42 +0100

    renames config_evaluator to evaluators

--- brains/evaluator-methods.js
+++ brains/evaluator-methods.js
@@ -1,6 +1,6 @@
 export function runEvaluator(arrayRobots,config_file){
   if(config_file){
-    var evaluator = require("../assets/config_evaluator/"+config_file);
+    var evaluator = require("../assets/evaluators/"+config_file);
     evaluator.main(arrayRobots);
   }
 }
