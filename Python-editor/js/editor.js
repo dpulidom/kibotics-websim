7daf4c13ea7f82b411e527e8f5021f326b25f298
sergiopaniego <sergiopaniegoblanco@gmail.com>
2019-11-04 12:56:01 +0100

    Python-editor adapted to Websim-2.0

--- Python-editor/js/editor.js
+++ Python-editor/js/editor.js
@@ -1,12 +1,14 @@
-import {setupACE, toggleCameraDisplay, getCode, insertCode, reset,changeSpectatorCamera} from './editor-methods.js'
+import editor from './editor-methods.js'
+import brains from '../../brains/brains-methods.js'
 
-var editor = null
+var editorRobot1 = 'a-pibot';
+var editorRobot2 = 'alvaro-robot';
 
-$(document).ready(()=>{
-  editor = setupACE();
+$(document).ready(async ()=>{
+  editor.setup();
 
   $("#cambtn").click(()=>{
-    toggleCameraDisplay();
+    editor.toggleCamera();
   });
 
   $("#spectatorCamera").click(()=>{
@@ -14,27 +16,46 @@ $(document).ready(()=>{
   });
 
   $("#runbtn").click(()=>{
-    var pythonCodeString = getCode(editor);
-    const request = new Request('http://127.0.0.1:8000/get_python_to_javascript_code', {method: 'POST', body: '{"python_code": "' + pythonCodeString +'"}'});
+    /**
+     * Function to execute when run button clicked, multiple options
+     * supported:
+     * - Creates thread for a robot if not exists and runs
+     * - Stop thread for a robot if exists and running
+     * - Resume thread for a robot if exists and not running
+     */
+    var pythonCodeString = editor.getCode()
+    console.log(pythonCodeString);
+    const request = new Request('http://127.0.0.1:8000/backend/get_python_to_javascript_code', {method: 'POST', body: '{"python_code": "' + pythonCodeString +'"}'});
     fetch(request)
       .then(response => {
         return response.text();
       }).then(function(javascriptCodeString) {
-        var websimEvent = new CustomEvent('code-to-run', {
-          'detail': {
-            'code': javascriptCodeString
+        console.log(javascriptCodeString)
+        if (brains.threadExists(editorRobot1)) {
+          if (brains.isThreadRunning(editorRobot1)) {
+            brains.stopBrain(editorRobot1);
+          } else {
+            brains.resumeBrain(editorRobot1, javascriptCodeString);
           }
-        });
-        document.dispatchEvent(websimEvent);
+        } else {
+            brains.runBrain(editorRobot1,javascriptCodeString);
+        }
       }).catch(error => {
       console.error(error);
     });
   });
 
   $('#resetRobot').click(()=>{
-    var resetEvent = new CustomEvent('reset', {
-      'detail': ''
-    });
-    document.dispatchEvent(resetEvent);
+    editor.sendEvent('reset');
   });
+
+  $('#simButton').click(()=>{
+    Websim.simulation.toggleSimulation();
+  });
+
+  // Init Websim simulator with config contained in the file passed
+  // as parameter
+  await Websim.config.init(config_file);
+
+  //setInterval(brains.showThreads, 1000);
 });
