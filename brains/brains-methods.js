42c0acdbb595cc181377296f2d4d6d1850e2f0e9
sergiopaniego <sergiopaniegoblanco@gmail.com>
2019-11-28 13:51:37 +0100

    while true for PyOnBrowser

--- brains/brains-methods.js
+++ brains/brains-methods.js
@@ -5,34 +5,52 @@ const setIntervalSynchronous = utils.setIntervalSynchronous;
 var brains = {};
 brains.threadsBrains = [];
 
-function cleanCode(c) {
-  /* Remove anything subsequent to an infinite loop */
-  if (c.split("while(true)").length <= 2 && c.indexOf("while(true)") != -1) {
-    // only one infinite loop allowed
-    var loop = c.split("while(true)")[1];
-    var endwhile;
-    for (var char=0, ignore=-1; char < loop.length; char++) {
-      if (loop[char] == '{') {
-        // another JS sentence starting ({) found
-        ignore += 1;
-      } else if (loop[char] == '}') {
-        if (ignore > 0) {
-          // if another sentence starting was found
-          // this (}) is not the end of the infinite loop
-          ignore -= 1;
-        } else {
-          // end of infinite loop found
-          endwhile = char;
-          break;
-        }
+const PYTHON_WHILE = "while(true)";
+const PYONBROWSER_WHILE = "while ( (__PyTrue__).__bool__ () === __PyTrue__)";
+const START_USER_CODE = "// START USER CODE";
+const END_USER_CODE = "// END USER CODE";
+
+function getLoopEnd(loop) {
+  let endWhile;
+  for (var char=0, ignore=-1; char < loop.length; char++) {
+    if (loop[char] == '{') {
+      // another JS sentence starting ({) found
+      ignore += 1;
+    } else if (loop[char] == '}') {
+      if (ignore > 0) {
+        // if another sentence starting was found
+        // this (}) is not the end of the infinite loop
+        ignore -= 1;
+      } else {
+        // end of infinite loop found
+        endWhile = char;
+        break;
       }
     }
-    loop = loop.substring(0,endwhile+1); // removing anythinf after end of loop
-    // recovering the previous code and building the clean code
-    var newc = c.split("while(true)")[0] + "while(true)" + loop;
-    return newc;
+  }
+  if (endWhile+1 < loop.length-1) {
+    Swal.fire({
+      type: 'info',
+      text: 'Ese código no se ejecutará',
+      title: 'Detectado código después del bucle infinito',
+    });
+  }
+  return endWhile;
+}
+
+
+function cleanCode(code) {
+  /* Remove anything subsequent to an infinite loop */
+  if (code.split(PYTHON_WHILE).length <= 2 && code.indexOf(PYTHON_WHILE) != -1) {
+    // only one infinite loop allowed
+    var loop = code.split(PYTHON_WHILE)[1];
+    let endWhile = getLoopEnd(loop);
+    loop = loop.substring(0,endWhile+1); // removing anything after end of loop
+    // recovering the previous code and building the cleaned code
+    var newCode = code.split(PYTHON_WHILE)[0] + PYTHON_WHILE + loop;
+    return newCode;
   } else {
-    return c; 
+    return code;
   }
 }
 
@@ -41,23 +59,38 @@ brains.createTimeoutBrain = (code, myRobot, id)=>{
   stopTimeoutRequested = false;
   var iterative_code, sequential_code;
   // SI+ Applications
-  if (code.split("while(true)").length <= 2 && code.split("while ( (__PyTrue__).__bool__ () === __PyTrue__)").length <= 2) {
+  if (code.split(PYTHON_WHILE).length <= 2 && code.split(PYONBROWSER_WHILE).length <= 2) {
     // only one infinite loop allowed
-    if (code.indexOf("while(true)") != -1) {
-      if (code.split("while(true)")[0] == "async function myAlgorithm(){\n"){
+    if (code.indexOf(PYTHON_WHILE) != -1) {
+      if (code.split(PYTHON_WHILE)[0] == "async function myAlgorithm(){\n"){
         sequential_code = null;
       } else {
-        sequential_code = code.split("while(true)")[0] + '\n}\nmyAlgorithm();' // S
+        sequential_code = code.split(PYTHON_WHILE)[0] + '\n}\nmyAlgorithm();' // S
       }
-      iterative_code = "async function myAlgorithm()" + code.split("while(true)")[1]; // I
+      iterative_code = "async function myAlgorithm()" + code.split(PYTHON_WHILE)[1]; // I
       var pos = iterative_code.lastIndexOf('}');
       iterative_code = iterative_code.substring(0,pos) + '' + iterative_code.substring(pos+1);
+    } else if (code.indexOf(PYONBROWSER_WHILE) !== -1) {
+      let start_general_code = code.split(START_USER_CODE)[0];
+      let end_general_code = code.split(END_USER_CODE)[1];
+      let user_code = code.split(START_USER_CODE)[1];
+      user_code = user_code.split(END_USER_CODE)[0];
+      if (code.split(PYONBROWSER_WHILE)[0] === "async function myAlgorithm(){\n") {
+        sequential_code = null;
+      } else {
+        sequential_code = user_code.split(PYONBROWSER_WHILE)[0]
+        sequential_code = start_general_code + sequential_code + end_general_code;
+      }
+      var loop = user_code.split(PYONBROWSER_WHILE)[1];
+      let endWhile = getLoopEnd(loop);
+      loop = loop.substring(0,endWhile+1);
+      iterative_code = start_general_code + loop + end_general_code;
     } else {
       sequential_code = code;
       iterative_code = null;
     }
-    //console.log('sequential:\n'+sequential_code);
-    //console.log('iterative:\n'+iterative_code);
+    // console.log('sequential:\n'+sequential_code);
+    // console.log('iterative:\n'+iterative_code);
 
     let brainIteration = setTimeout(async function iteration(){
       if (sequential_code != null) {
@@ -78,12 +111,12 @@ brains.createTimeoutBrain = (code, myRobot, id)=>{
   } else {
     Swal.fire({
       type: 'error',
-      text: 'Oops...',
+      text: 'Modifica el código y vuelve a ejecutar',
       title: 'Error en el código.\nSólo puedes poner un bucle infinito',
     });
     return undefined;
   }
-}
+};
 
 brains.runBrain = (robotID, code) =>{
   /**
@@ -101,11 +134,11 @@ brains.runBrain = (robotID, code) =>{
     "iteration": brains.createTimeoutBrain(code, Websim.robots.getHalAPI(robotID), robotID),
     "codeRunning": code
   });
-}
+};
 
 brains.threadExists = (robotID)=>{
   return brains.threadsBrains.find((threadBrain)=> threadBrain.id == robotID);
-}
+};
 
 brains.isThreadRunning = (robotID)=>{
   /**
@@ -115,7 +148,7 @@ brains.isThreadRunning = (robotID)=>{
    */
   var threadBrain = brains.threadsBrains.find((threadBrain)=> threadBrain.id == robotID);
   return threadBrain.running;
-}
+};
 
 brains.resumeBrain = (robotID, code) =>{
   code = cleanCode(code);
@@ -124,7 +157,7 @@ brains.resumeBrain = (robotID, code) =>{
   threadBrain.iteration = brains.createTimeoutBrain(code, Websim.robots.getHalAPI(robotID), robotID);
   threadBrain.running = true;
   threadBrain.codeRunning = code;
-}
+};
 
 brains.stopBrain = (robotID) =>{
   /**
@@ -134,7 +167,7 @@ brains.stopBrain = (robotID) =>{
   stopTimeoutRequested = true;
   clearTimeout(threadBrain.iteration);
   threadBrain.running = false;
-}
+};
 
 brains.showThreads = ()=>{
   /**
@@ -143,6 +176,6 @@ brains.showThreads = ()=>{
   brains.threadsBrains.forEach((threadBrain)=>{
     console.log(threadBrain);
   })
-}
+};
 
 module.exports = brains;
